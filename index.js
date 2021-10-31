3dd6bb8bfd61d5fcf4a2c558c880e98ac5e87e08
Tohir Raihan <tohir.raihan@gmail.com>
2021-10-31 16:11:09 +0600

    delete order api added

--- index.js
+++ index.js
@@ -64,6 +64,14 @@ async function run() {
             const result = await orderCollection.insertOne(newOrder);
             res.json(result);
         });
+
+        // DELETE order
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        });
     } finally {
         // await client.close();
     }
